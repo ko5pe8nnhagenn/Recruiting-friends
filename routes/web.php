29f98a29c92815a0ef0563b626663c21b57ca895
ko5pe8nnhagenn <su0zu5ka0su8@gmail.com>
2021-12-06 11:45:14 +0000

    適切なコミットコメント

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Routes;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
+Route::group(['middleware' => 'auth'], function(){
 Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}/edit', 'PostController@edit');
@@ -19,7 +21,8 @@ Route::put('/posts/{post}', 'PostController@update');
 Route::delete('/posts/{post}', 'PostController@delete');
 Route::get('/posts/{post}', 'PostController@show');
 Route::post('/posts', 'PostController@store');
-Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/categories/{category}', 'CategoryController@index');
 Route::get('/user', 'UserController@index');
+});
